c7f2b1d1b717ea831beb0e388a4d1d97da22c258
“arrahman” <arrahman1003@gmail.com>
2023-04-10 16:19:12 +0600

    .

---
+++ class 8.py
@@ -0,0 +1,12 @@
+#a=int(input("inter a new number"))
+#b=int(input("inter a another number"))
+#x=a+b
+#y=a-b
+#z=a/b
+#print(x)
+#print(y)
+#print(z)
+a=input("What is your name")
+print("Nice to meet you "+a)
+
+
